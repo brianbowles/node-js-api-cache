d50514b237ac69875f9571a82f5809392df67c7f
tmptest1 <mrbbowles@gmail.com>
2015-06-29 02:06:46 -0500

    First version .. no history .. minimal testing

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var https   = require('https');
 var qs      = require('querystring');
 var crypto  = require('crypto');
 var _       = require('underscore');
+var moment  = require('moment');
 
 // API config
 var apiOptions = {
-  hostname: 'api.github.com',
+  hostname: 'www.bitstamp.net',
   port: 443,
-  auth: process.env.GITHUB_APPLICATION_KEY, // 'username:pass'
+  path: '/api/ticker/',
+//  auth: process.env.GITHUB_APPLICATION_KEY, // 'username:pass'
   headers: {'User-agent' : 'node-api-cache'}
 };
 
@@ -21,13 +23,28 @@ var options = {
   nodePort: 3001,           // port to start server on
   rateLimit: true,          // throttle requests, bool
   rateLimitValue: 20,       // quota: req per time unit
-  rateLimitPeriod: 'minute' // quote: time for quota refresh: 'second', 'minute', 'hour'
+  rateLimitPeriod: 'minute', // quote: time for quota refresh: 'minute', 'hour' , 'day' .. second not supported
+  dataElement:'last'
 };
 
 // Rate limiter - optional
 if (options.rateLimit) {
   var RateLimiter = require('limiter').RateLimiter;
   var limiter     = new RateLimiter(options.rateLimitValue, options.rateLimitPeriod);
+
+  var perSecond = 0;
+  var secondsPerLimiter = 0;
+  if (options.rateLimitPeriod == 'minute') {
+      secondsPer = 60;
+  }  else if (options.rateLimitPeriod == 'hour') {
+      secondsPer = 3600;
+  }  else if (options.rateLimitPeriod == 'day') {
+      secondsPer = 3600 * 24;
+  } else {
+      console.log("Invalid rateLimitPeriod");
+      exit(1);
+  }
+  secondsPerLimiter = secondsPer / options.rateLimitValue;
 }
 
 // NodeCache memory cache - optional
@@ -47,36 +64,39 @@ var app = express();
 app.enable("jsonp callback");
 
 app.listen(options.nodePort);
-console.log("Starting API cache for %s. Listening on port 3000.", apiOptions.hostname);
+console.log("Starting API cache for %s. Listening on port " + options.nodePort, apiOptions.hostname);
 
 app.use(function(req, res, next){
 
-  // ignore favicon requests from browser
-  if (req.url === '/favicon.ico') { 
-    res.send('OK');
-    return;
-  }
+    //console.log(req);  
+    var callbackName = req.param('callback');
+
+    // So we need a key that is changed once per time window during the limiter
+    // strip out millisecs .. then put them back in as needed
+    var curDate = new Date();
+    var keyDate = moment(Math.round((curDate) / 1000)*1000);  // remove sub-second resolution
+
+    // See http://momentjs.com/docs/#/displaying/ this allows complete customization of output string
+    // have the key not change until a new time period
+    var key = moment(keyDate - (((keyDate / 1000) % secondsPerLimiter))*1000).format();
+
+    var tick = Math.floor(keyDate / (secondsPerLimiter * 1000));
+
+    //console.log("secondsPerLimiter" + " = " + secondsPerLimiter + " --- " + (keyDate % secondsPerLimiter));
+    //console.log(moment(keyDate).format() + " --- " + key);
+
+    var filePath     = "./data/" + key + '.json';
   
-  var callbackName = req.param('callback');
-  var key          = generateKey();
-  var filePath     = "./data/" + key + '.json';
-  
-  // Check cache
-  (options.memCache) ? tryCache() : tryFileSystem();
-  
-  function generateKey() {
-    var md5sum = crypto.createHash('md5');
-    md5sum.update(req.path + '?' + refineQueryString(req.query));
-    return md5sum.digest('hex');
-  }
+    // Check cache
+    (options.memCache) ? tryCache() : tryFileSystem();
 
   function tryCache() {
-    cache.get(key, function(err, value){
+    cache.get(key, function(err, value) {
       if(_.isEmpty(value)){
-        console.log('cache MISS: %s %s', req.method, req.url);
+        console.log('cache MISS');
         startRequest();
       } else {
-        console.log('cache HIT: %s %s', req.method, req.url);
+        console.log('cache HIT');
         respondToClient(value[key]);
       }
     });
@@ -95,16 +115,12 @@ app.use(function(req, res, next){
   }
 
   function startRequest() {
-    apiOptions.path = req.originalUrl;
 
-    if (options.rateLimit) {
-      limiter.removeTokens(1, function(err, remainingReq) {
-        console.log("API requests left this %s: %s", options.rateLimitPeriod,remainingReq);
-        makeApiRequest();
-      });
-    } else {
+    limiter.removeTokens(1, function(err, remainingReq) {
+      console.log("API requests left this %s: %s", options.rateLimitPeriod,remainingReq);
       makeApiRequest();
-    }
+    });
+
   }
 
   function makeApiRequest() {
@@ -122,10 +138,27 @@ app.use(function(req, res, next){
       return;
     });
   }
+  // add data to the original oracles output
+  // tick & time
+  function addNeurealJSON(output) {
+      var obj = JSON.parse(output);
+      var retObj = {};
+
+      retObj['data'] = obj[options.dataElement];
+      retObj['tick'] = tick;
+      retObj['date'] = key;
+
+      return JSON.stringify(retObj);
+  }
 
   function apiResponseEnd(output) {
-    output = stripCallback(output); // remove JSONP callback from cached version
-    
+
+    console.log("output before = " + output);
+      //output = stripCallback(output); // remove JSONP callback from cached version
+     
+    output = addNeurealJSON(output);
+    console.log("output after = " + output);
+
     (options.memCache) ? saveToCache(output) : saveToFile(output);
     
     respondToClient(output);
@@ -155,10 +188,17 @@ app.use(function(req, res, next){
     });
   }
 
+  function pruneMemCache() {
+      // TODO
+  }
+
   function respondToClient(data) {
     res.set({"Content-Type": "application/json"});
-    res.write(callbackName + '(' + data + ')'); // JSON -> JSONP
+
+    //res.write(callbackName + '(' + data + ')'); // JSON -> JSONP
+    res.write(data);
     res.end();
+    pruneMemCache();
   }
 
   function refineQueryString(reqQuery) {
